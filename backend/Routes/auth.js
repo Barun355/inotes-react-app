d245a963d3340ed01c36d2faf2cae3a698faf539
Barun355 <baruntiwary620@gmail.com>
2022-11-20 22:55:27 +0530

    Refactor the auth router by adding some error handling technique, well comments, validations

--- backend/Routes/auth.js
+++ backend/Routes/auth.js
@@ -1,33 +1,55 @@
+// importing the express module to create the backend API for our iNotes app {npm i express to install the express}
 const express = require("express");
+
+// initiating the Router method of the express. It is used to create the new routes or manage the new end points in express.
 const router = express.Router();
+
+// creating the user model instance to use it to create the user after geting the post request to the end point of this api /api/auth/createUser
 const User = require('../Models/Users');
-const {body, validationResult} = require('express-validator')
 
 
-// create a user using: POST "/api/auth". Dosen't require asuth
-router.get('/',[
-    body('name', 'The name should be atleast more than 3 letter').isLength({min: 3}),
+// importing the body and validationResult method from the express-validator module for validation of the data
+// npm i -D express-validator { for Dev Dependencies}
+const { body, validationResult } = require('express-validator')
+
+
+// create a user using: POST request to the end point "/api/auth/createUser". This end point dosen't require the authenticaiton.
+// validating the name, email and password, adding the custom error for invalid input.
+router.post('/createUser', [
+    body('name', 'The name should be atleast more than 3 letter').isLength({ min: 3 }),
     body('email', 'Enter the correct email.').isEmail(),
-    body('password', 'The length of the password must be more that 5').isLength({min: 5}),
-], (req, res)=>{
-    const error = validationResult(req);
+    body('password', 'The length of the password must be more that 5').isLength({ min: 5 }),
+], async (req, res) => {
 
-    if(!error.isEmpty()){
-        return res.status(400).json({errors: error.array()});
+    try {
+        // getting the result of the request to this end point and storing it to the constant as an error to throw error if any.
+        const error = validationResult(req);
+
+        // checking if their is any error and send the response according to it
+        if (!error.isEmpty()) {
+            return res.status(404).json({ errors: error.array() });
+        }
+
+        // validating if the user already exist. And sending the response according to it.
+        let user = await User.findOne({ email: req.body.email });
+        if (user) {
+            console.log(user);
+            return res.status(404).json({ error: "Sorry a user already exist with this email" });
+        }
+
+        // creating user if the user not exist.
+        user = await User.create({
+            name: req.body.name,
+            email: req.body.email,
+            password: req.body.password,
+        })
+
+        // sending the response in the form of json.
+        res.json(user)
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send("Some error occured.");
     }
-    // res.send(req.body);
-    // console.log(req.body);
-    // const user = User(req.body);
-    // user.save();
-    // console.log(req.body.email);
-    User.create({
-        name: req.body.name,
-        email: req.body.email,
-        password: req.body.password,
-    }).then(user => res.json(user)).catch( err=> {
-        console.log(err);
-        res.json({err: "Enter the valid email", message: err.message});
-    })
 })
 
 
